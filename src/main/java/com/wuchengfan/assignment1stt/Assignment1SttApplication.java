30b1d40af8bbabdfcaf3f62bdaa73b9f9408150a
wuchengFan <a3011093@adelaide.edu.au>
2026-09-07 23:07:55 +0930

    Initial Spring Boot project setup

---
+++ src/main/java/com/wuchengfan/assignment1stt/Assignment1SttApplication.java
@@ -0,0 +1,13 @@
+package com.wuchengfan.assignment1stt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1SttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1SttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuchengfan/assignment1stt/Assignment1SttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuchengfan.assignment1stt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1SttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
